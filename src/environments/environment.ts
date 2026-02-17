013978242bf141d5adabbbd5b9cb7915b42bced8
Eduardo Rodriguez <edro280195@gmail.com>
2026-02-17 13:55:02 -0600

    Cambio url para pedidos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'https://regibazarapi.onrender.com/api',
   hubUrl: 'https://regibazarapi.onrender.com/hubs/tracking',
-  frontendUrl: 'http://localhost:4200'
+  frontendUrl: 'https://regibazar.com'
 };
