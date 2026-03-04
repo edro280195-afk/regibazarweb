d53374b820906a02b5b67a28324ac67e5f11f49a
Eduardo Rodriguez <edro280195@gmail.com>
2026-03-03 19:03:13 -0600

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:56164/api',//'https://regibazarapi.onrender.com/api',
-  hubUrl: 'https://localhost:56164/hubs/tracking/api',//'https://regibazarapi.onrender.com/hubs/tracking/api',
+  apiUrl: 'https://regibazarapi.onrender.com/api',
+  hubUrl: 'https://regibazarapi.onrender.com/hubs/tracking/api',
   frontendUrl: 'https://regibazar.com',
   googleMapsApiKey: 'AIzaSyD0h3ou4iWXQtthgXE-LMewALcWpYdzzJk',
 };
