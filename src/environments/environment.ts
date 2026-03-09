b1116c7bc0161cb397e99f052d5f4ef5d78bd84a
Eduardo Rodriguez <edro280195@gmail.com>
2026-03-09 17:35:26 -0500

    cambio en ruta api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:56164/api',
-    hubUrl: 'https://localhost:56164/hubs/delivery',
+    apiUrl: 'https://regibazarapi.onrender.com/api',
+    hubUrl: 'https://regibazarapi.onrender.com/hubs/delivery',
     googleMapsApiKey: 'AIzaSyD0h3ou4iWXQtthgXE-LMewALcWpYdzzJk',
     vapidPublicKey: 'BNR4b92lH7N43gqzFmXlXmNmsw09EvIiNAyhqKj0j1M66oVMXYX3MRFB3fec3iOrupzPnWe1l7k9Un8o_itTJYs'
 };
