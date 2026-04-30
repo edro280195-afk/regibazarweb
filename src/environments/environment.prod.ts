9dbc05282df522c965e865c75d9e804feb279bf2
Eduardo Rodriguez <edro280195@gmail.com>
2026-04-30 01:14:56 -0500

    cambio url publica mp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,5 +6,5 @@ export const environment = {
     hubUrl: 'https://regibazarapi.onrender.com/hubs/delivery',
     googleMapsApiKey: 'AIzaSyD0h3ou4iWXQtthgXE-LMewALcWpYdzzJk',
     vapidPublicKey: 'BNR4b92lH7N43gqzFmXlXmNmsw09EvIiNAyhqKj0j1M66oVMXYX3MRFB3fec3iOrupzPnWe1l7k9Un8o_itTJYs',
-    mpPublicKey: 'TEST-bbc97906-faf4-4784-ae3a-d176dd0bd06e'
+    mpPublicKey: 'APP_USR-63d6e9c9-b3fd-4052-9679-acf595f96aef'
 };
